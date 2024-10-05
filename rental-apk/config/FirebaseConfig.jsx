0384733ef3d95dc3b0766994f11255ceb635ac92
asqre <amitanand.asqre@gmail.com>
2024-10-06 03:18:34 +0530

    firebase config

---
+++ rental-apk/config/FirebaseConfig.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPzgxvTZ_dTqYS7XgsJoWsSQQo-130g7k",
+  authDomain: "rental-master-eac37.firebaseapp.com",
+  projectId: "rental-master-eac37",
+  storageBucket: "rental-master-eac37.appspot.com",
+  messagingSenderId: "517942295053",
+  appId: "1:517942295053:web:b5eeb0179bff7a6f5ff344",
+  measurementId: "G-CJRNKZSHM7",
+};
+
+export const app = initializeApp(firebaseConfig);
