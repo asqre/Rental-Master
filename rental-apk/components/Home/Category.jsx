f94b41a137f9294e87d27dbe06a926971ce0d294
asqre <amitanand.asqre@gmail.com>
2024-10-06 04:20:05 +0530

    category item added

---
+++ rental-apk/components/Home/Category.jsx
@@ -0,0 +1,72 @@
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Colors } from "../../constants/Colors";
+import { query, collection, getDocs } from "firebase/firestore";
+import CategoryItem from "./CategoryItem";
+import { db } from "../../config/FirebaseConfig";
+
+export default function Category() {
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    GetCategoryList();
+  }, []);
+
+  const GetCategoryList = async () => {
+    try {
+      setCategoryList([]);
+      const q = query(collection(db, "Category"));
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((doc) => {
+        setCategoryList((prev) => [...prev, doc.data()]);
+      });
+    } catch (error) {
+      console.error("Error getting Category:: ", error);
+    }
+  };
+
+  return (
+    <View>
+      <View
+        style={{
+          padding: 20,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginTop: 10,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "outfit-bold",
+          }}
+        >
+          Category
+        </Text>
+        <Text style={{ color: Colors.PRIMARY, fontFamily: "outfit-medium" }}>
+          View All
+        </Text>
+      </View>
+
+      <FlatList
+        data={categoryList}
+        renderItem={({ item, Index }) => (
+          <CategoryItem
+            category={item}
+            key={Index}
+            onCategoryPress={(category) => {
+              console.log(category);
+            }}
+          />
+        )}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={{
+          marginLeft: 20,
+        }}
+      />
+    </View>
+  );
+}
