6ea0bb91d5ea739806ccfeea31481aa0ea5f4fb5
asqre <amitanand.asqre@gmail.com>
2024-10-07 22:36:20 +0530

    remove default icon and splash screen

--- rental-apk/app/_layout.jsx
+++ rental-apk/app/_layout.jsx
@@ -3,6 +3,8 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import LoginScreen from "./../components/LoginScreen";
 import * as SecureStore from "expo-secure-store";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 const tokenCache = {
   async getToken(key) {
@@ -21,12 +23,25 @@ const tokenCache = {
   },
 };
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  useFonts({
-    "outfit": require("./../assets/fonts/Outfit-Regular.ttf"),
+  const [loaded, error] = useFonts({
+    'outfit': require("./../assets/fonts/Outfit-Regular.ttf"),
     "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
     "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
   });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <ClerkProvider
       tokenCache={tokenCache}
