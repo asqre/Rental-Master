58386692b8fd9349255eb0718a0ec0a1f5a61bce
asqre <amitanand.asqre@gmail.com>
2024-10-07 18:25:27 +0530

    add share and download

--- rental-apk/components/Profile/MenuList.jsx
+++ rental-apk/components/Profile/MenuList.jsx
@@ -1,9 +1,18 @@
-import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Share,
+} from "react-native";
 import React from "react";
 import { Colors } from "../../constants/Colors";
 import { useRouter } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function MenuList() {
+  const { signOut } = useAuth();
   const router = useRouter();
 
   const menuList = [
@@ -23,17 +32,29 @@ export default function MenuList() {
       id: 1,
       name: "Share App",
       icon: require("./../../assets/images/share_1.png"),
-      path: "",
+      path: "share",
     },
     {
       id: 1,
       name: "Logout",
       icon: require("./../../assets/images/logout.png"),
-      path: "",
+      path: "logout",
     },
   ];
 
   const onMenuClick = (item) => {
+    if (item.path === "logout") {
+      signOut();
+      return;
+    }
+
+    if (item.path === "share") {
+      Share.share({
+        message:
+          "Download the Business Directory app, Download Url: https://play.google.com/store/apps/details?id=com.asqre.businessdirectory",
+      });
+      return;
+    }
     router.push(item.path);
   };
 
