dd9412ea64ca6c0d553ab7bd8b8cbf0203191e38
Shantanu-2127 <Shantanusisodia777@gmail.com>
2024-10-16 23:17:10 +0530

    fixedColor

--- user/src/pages/Home.js
+++ user/src/pages/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import Section from "../section/Section";
 import Banner from "../section/Banner";
+import RoadMap from "../section/RoadMap";
 
 const Home = () => {
   return (
     <>
       <Layout>
         <Banner />
-        <Section />
+        {/* <Section /> */}
+        <RoadMap />
       </Layout>
     </>
   );
