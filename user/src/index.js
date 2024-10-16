ace77e9d32f0d9f5ef49ffac99876538d3663ac4
asqre <amitanand.asqre@gmail.com>
2024-10-16 17:14:16 +0530

    added slider at the landing page

--- user/src/index.js
+++ user/src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { DataProvider } from "./context/data";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- user/src/section/Banner.js
+++ user/src/section/Banner.js
@@ -1,13 +1,43 @@
 import React from "react";
 import { useData } from "../context/data";
+import Slider from "react-slick";
 
 const Banner = () => {
   const { bannerImages } = useData();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    adaptiveHeight: false,
+  };
+
   return (
-    <div>
-        
+    <div className="relative mb-6 md:0">
+      {Array.isArray(bannerImages) && bannerImages.length > 0 ? (
+        <Slider {...settings}>
+          {bannerImages?.map((img) => (
+            <img
+              key={img}
+              src={img}
+              alt="Banner Images"
+              height={724}
+              width={1400}
+              className="object-cover h-[300px] md:h-[762px]"
+            />
+          ))}
+        </Slider>
+      ) : (
+        <div className="h-[300px] md:h-[762px] bg-slate-400" />
+      )}
     </div>
-  )
+  );
 };
 
 export default Banner;
