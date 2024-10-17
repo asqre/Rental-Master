0f52466eb4e39a59944dacffe0ede7a59cab3930
asqre <amitanand.asqre@gmail.com>
2024-10-17 21:34:22 +0530

    added about section

--- user/src/pages/Home.js
+++ user/src/pages/Home.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
-import Section from "../section/Section";
 import Banner from "../section/Banner";
 import RoadMap from "../section/RoadMap";
 import FeatureBenefits from "../section/FeatureBenefits";
+import About from "../section/About";
 
 const Home = () => {
   return (
     <>
       <Layout>
         <Banner />
-        {/* <Section /> */}
+        <About />
         <RoadMap />
         <FeatureBenefits />
       </Layout>
