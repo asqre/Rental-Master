647148b6939d5c80708c0dabdc0cbd516e354a80
asqre <amitanand.asqre@gmail.com>
2024-10-18 08:57:04 +0530

    fix css in about section

--- user/src/section/About.js
+++ user/src/section/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section
       id="about"
-      className="flex flex-col gap-[32px] md:flex-row w-full mx-auto px-[5%] md:mx-0 mb-10"
+      className="flex flex-col gap-[32px] md:flex-row w-full mx-auto px-[5%] md:mx-0 mb-10 py-[5rem]"
     >
       <div className="md:w-[50%]">
         <img src={aboutImg} alt="about us" />
@@ -13,9 +13,9 @@ const About = () => {
 
       <div className="flex flex-col gap-[14px] sm:gap-[20px] md:gap-[48px] md:w-[50%]">
         <h2>
-          <span className="text-[#222222]">We think</span>
+          <span className="text-[#222222]">About</span>
           <br />
-          <span className="text-[#1961FD]">differently</span>
+          <span className="text-[#1961FD]">Us</span>
           <span className="text-[#222222]">.</span>
         </h2>
 
@@ -27,13 +27,13 @@ const About = () => {
             everyday errands, our fleet of well-maintained cars is ready to take
             you wherever you need to go.
             <br />
-            <br/>
+            <br />
             With a wide range of vehicles from compact cars to luxury sedans and
             SUVs, we ensure that you find the perfect ride for your journey. Our
             rental process is quick, simple, and hassle-free, offering flexible
             booking options and affordable pricing.
             <br />
-            <br/>
+            <br />
             At Rental Master, we prioritize customer satisfaction by providing
             24/7 customer support and ensuring that each rental vehicle is
             clean, safe, and in excellent condition. Let us be your trusted
