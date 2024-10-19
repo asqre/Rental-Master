d0c093013a375729a6727ff409fbbd3d4635afe6
asqre <amitanand.asqre@gmail.com>
2024-10-19 15:51:36 +0530

    remove content basic

--- user/src/components/common/Logo.js
+++ user/src/components/common/Logo.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <a href="/" className="flex flex-row space-x-2 items-center justify-center">
+    <Link to="/" className="flex flex-row space-x-2 items-center justify-center">
       <img
         src="https://imgd.aeplcdn.com/370x208/n/cw/ec/130591/fronx-exterior-right-front-three-quarter-109.jpeg?isig=0&q=80"
         alt="logo"
         className="h-10 w-20"
       />
       <div className="text-xl font-normal text-gray-900">Rental Masters</div>
-    </a>
+    </Link>
   );
 };
 
