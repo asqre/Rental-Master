e6fea46907b831b87840da5e7549a080b9bdcebd
asqre <amitanand.asqre@gmail.com>
2024-10-20 21:30:29 +0530

    fixed navigation to base url

--- user/src/pages/Home.js
+++ user/src/pages/Home.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import bg_home from "../assets/bg_home.webp";
+import { useData } from "../context/data";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { city } = useData();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (city) {
+      navigate(`/${city}/car-rentals`);
+    }
+  }, [city]);
+
   return (
     <div className="w-[100vw] h-[100vh]">
       <img
