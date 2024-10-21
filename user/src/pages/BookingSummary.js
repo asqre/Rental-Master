7e7b3e4a270bf4ae0a105f0555981ffce13b5213
asqre <amitanand.asqre@gmail.com>
2024-10-21 17:07:01 +0530

    fixed css

--- user/src/pages/BookingSummary.js
+++ user/src/pages/BookingSummary.js
@@ -3,20 +3,23 @@ import CarProperties from "../components/bookingSummary/CarProperties";
 import CarDetails from "../components/bookingSummary/CarDetails";
 import BookingTime from "../components/bookingSummary/BookingTime";
 import Checkout from "../components/bookingSummary/Checkout";
+import Layout from "../components/layout/Layout";
 
 const BookingSummary = () => {
   return (
-    <div className="flex flex-row gap-5 w-[100%] h-[100%] p-10">
-      <div className="flex flex-col gap-10 w-[60%]">
-        <CarProperties />
-        <BookingTime />
-        <CarDetails />
-      </div>
+    <Layout>
+      <div className="flex flex-col lg:flex-row gap-5 w-[100%] h-[100%] p-10">
+        <div className="flex flex-col gap-10 w-[100%] lg:w-[70%]">
+          <CarProperties />
+          <BookingTime />
+          <CarDetails />
+        </div>
 
-      <div className="w-[40%]">
-        <Checkout />
+        <div className="w-[100%] lg:w-[30%]">
+          <Checkout />
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
