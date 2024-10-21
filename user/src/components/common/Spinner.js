defdb511957fa2848b437f65c89d37806eac2a7a
asqre <amitanand.asqre@gmail.com>
2024-10-21 18:36:59 +0530

    added spinner

---
+++ user/src/components/common/Spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Spinner = ({ isLoading }) => {
+  return (
+    <div
+      className={`fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 ${
+        isLoading ? "block" : "hidden"
+      }`}
+    >
+      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+    </div>
+  );
+};
+
+export default Spinner;
