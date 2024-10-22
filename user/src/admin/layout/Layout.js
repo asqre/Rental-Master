254c62e31df6b6f3d0654a10d038de5efeb83570
asqre <amitanand.asqre@gmail.com>
2024-10-22 22:11:53 +0530

    added drawer

--- user/src/admin/layout/Layout.js
+++ user/src/admin/layout/Layout.js
@@ -1,29 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { routes } from "../routes";
 import SideNav from "./SideNav";
+import DrawerIcon from "../assets/Drawer.png";
+import { Drawer, ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme();
 
 const Layout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setIsOpen(open);
+  };
+
   return (
-    <div className="flex flex-row gap-7 h-screen w-screen p-[1rem] bg-adminBackground">
-      {/* Left layout */}
-      <div className="h-[100%]">
-        <SideNav />
+    <ThemeProvider theme={theme}>
+      <div className="flex flex-row gap-7 h-screen w-screen p-[1rem] bg-adminBackground">
+        <div className="absolute left-[27px] sm:hidden">
+          <button
+            onClick={toggleDrawer(true)}
+            className="focus:outline-none h-6 w-6 hover:bg-slate-100 rounded-md flex items-center justify-center"
+          >
+            <img src={DrawerIcon} alt="Drawer icon" />
+          </button>
+        </div>
+
+        {/* Left layout */}
+        <div className="h-[100%] hidden sm:block">
+          <SideNav />
+        </div>
+
+        {/* Main content */}
+        <div className="flex flex-col flex-grow h-[100%] w-full">
+          <Routes>
+            {routes.map(
+              ({ layout, pages }) =>
+                layout === "dashboard" &&
+                pages.map(({ path, element }) => (
+                  <Route key={path} path={path} element={element} />
+                ))
+            )}
+          </Routes>
+        </div>
       </div>
 
-      {/* Main content */}
-      <div className="flex flex-col flex-grow h-[100%] w-full">
-        <Routes>
-          {routes.map(
-            ({ layout, pages }) =>
-              layout === "dashboard" &&
-              pages.map(({ path, element }) => (
-                <Route key={path} path={path} element={element} />
-              ))
-          )}
-        </Routes>
-      </div>
-    </div>
+      <Drawer
+        anchor="left"
+        open={isOpen}
+        onClose={toggleDrawer(false)}
+        className="sm:hidden bg-adminBackground"
+      >
+        <SideNav />
+      </Drawer>
+    </ThemeProvider>
   );
 };
 
